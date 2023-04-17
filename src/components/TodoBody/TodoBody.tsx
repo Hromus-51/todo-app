76c08e0fcbf24a794209d537b084bacc94bb1754
Ilgiz <98454627+Hromus-51@users.noreply.github.com>
2023-04-17 10:03:08 +0300

    Update TodoBody.tsx

--- src/components/TodoBody/TodoBody.tsx
+++ src/components/TodoBody/TodoBody.tsx
@@ -10,49 +10,11 @@ import {
 } from '../../redux/todoSlice/slice';
 
 
-import { useEffect } from 'react';
-const a = [
-    {
-        id: '53cc67de-ae1b-4bea-bb86-c04c221aef01',
-        name: 'Complete online JavaScript course',
-        status: 'Completed',
-    },
-    {
-        id: 'f8f150ed-67cf-42b4-8ba7-830c8ecd886f',
-        name: 'Jog around the park 3x',
-        status: 'Active',
-    },
-    {
-        id: 'e5272592-d1ee-4987-b065-f2f90d3e5302',
-        name: '10 minutes meditation',
-        status: 'Active',
-    },
-    {
-        id: '509b27ff-c6b1-4a00-a933-7984e058a60a',
-        name: 'Read for 1 hour',
-        status: 'Active',
-    },
-    {
-        id: '9818fe60-9157-4df6-ac1e-131090144464',
-        name: 'Pick up groceries',
-        status: 'Active',
-    },
-    {
-        id: '133f722a-e7c3-40df-a1d6-1f91b03c1770',
-        name: 'Complete Todo App on Frontend Mentor',
-        status: 'Active',
-    },
-]
-
 const TodoBody = () => {
     const dispatch = useDispatch();
     const todoList = useSelector(selectTodoList);
     const sort = useSelector(selectSortItem);
     const sortedList = todoList.filter(obj => obj.status === sort || sort === 'All');
-
-    useEffect(() => {
-        dispatch(setTodoList(a))
-    }, [])
 
     const handleOrder = (newList: TodoState["todoList"]) => {
         sort === 'All' ?
